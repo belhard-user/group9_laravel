22a25942a6dee780c7cd523d574ca575b79c10b0
unknown <ldarik2l@gmail.com>
2016-12-17 20:44:28 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,54 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'profile'], function($route){
+    $route->get('home', function(){
+        return "hello home";
+    });
+    $route->get('edit', function(){
+        return "hello edit";
+    });
+    $route->get('pwd', function(){
+        return "hello pwd";
+    });
+});
+
+Route::get('hello/{name?}/{from?}', function($name='Guset', $from='Admin'){
+    return title_case("Hello $name from $from");
+})->where([
+    'name' => '\w+',
+    'from' => '\w+'
+]);
+
+
+/*Route::match(['get', 'put', 'delete'], 'hello', function(){
+    $names = [
+        'Neo',
+        'Morpheus',
+        'Tank',
+        'Trinity',
+        'Dozer',
+        'Smith'
+    ];
+
+    return $names;
+});*/
+
+Route::any('hello2', function(){
+    $names = [
+        'Neo',
+        'Morpheus',
+        'Tank',
+        'Trinity',
+        'Dozer',
+        'Smith'
+    ];
+
+    $url = url('hello', [
+        'name' => $names[ array_rand($names) ],
+        'from' => $names[ array_rand($names) ]
+    ]);
+
+    return "<a href='$url'>click</a>";
+});
