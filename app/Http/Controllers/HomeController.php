e14a1634ebd801859178367e1ae54bee2215621d
unknown <ldarik2l@gmail.com>
2016-12-24 19:35:17 +0200

    migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 
 class HomeController extends Controller
 {
@@ -17,6 +18,7 @@ class HomeController extends Controller
             'Dozer',
             'Smith'
         ];
+
         return view('home.index', compact('names'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix' => 'profile', 'namespace' => 'Profile'], function(){
     Route::get('home', ['uses' => 'IndexController@index', 'as' => 'profile.main']);
 });
